6749c57be006ea07423be728264683270de434e8
Coffee-and-Me <kirich051@gmail.com>
2024-11-25 12:27:53 +0300

    Update laba4_5.py

--- laba4/laba4_5.py
+++ laba4/laba4_5.py
@@ -1,24 +1,22 @@
-def check(num: str):
-    try:
-        num = int(num)
-    except ValueError:
-        return False
-    else:
-        if num < 0:
-            return False
-        return True
+def nod(a, b):
+    if a == b:
+        return a
+    if a > b:
+        return nod(a - b, b)
+    return nod(a, b - a)
 
-def nok(a,b):
-    pass
+def nok(a, b):
+    return (a / nod(a, b)) * b
+
 
 a = b = 'e'
 while not check(a):
-    a = input('Введите число для конвертации: ')
+    a = input('Введите число: ')
 
 while not check(b):
-    b = input('Введите число для конвертации: ')
+    b = input('Введите число: ')
 
 a = int(a)
 b = int(b)
 
-print()
+print(nok(a, b))
