e9af99813df7739ec11efffb2bca28184893e3af
Kirill <kirich051@gmail.com>
2024-11-25 03:39:19 +0300

    upd

--- laba1/task1.py
+++
@@ -1,9 +0,0 @@
-oklad = 'a'
-while not oklad.isdigit():
-    oklad = input('Введите оклад: ')
-
-premia = int(oklad) * (2 / 3)
-stonks = int(oklad) * 0.87
-
-print(f"Премия составила: {premia}")
-print(f"Доход с учётом налогового вычета: {stonks}")
