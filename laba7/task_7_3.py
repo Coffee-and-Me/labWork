f6e7c17806c3fc0673aabfa4816b7c42a56f8e36
Coffee-and-Me <kirich051@gmail.com>
2024-12-03 12:06:40 +0300

    Update task_7_3.py

---
+++ laba7/task_7_3.py
@@ -0,0 +1,31 @@
+ef get_val(dictr, n):
+    for key, val in dictr.items():
+        if val == n:
+            return key
+
+
+while 1:
+    st = input('Последовательность через пробел: ')
+    try:
+        st = list(map(int, st.split()))
+    except ValueError:
+        print('error')
+    else:
+        break
+
+st = sorted(st)
+
+dictr = {}
+
+for i in range(len(st)):
+    dictr[st[i]] = st.count(st[i])
+
+lst = []
+for key, value in dictr.items():
+    lst.append(value)
+
+lst = sorted(lst)
+if lst[-1] == lst[-2]:
+    print('Моды не существует')
+else:
+    print(f'Мода последовательности {get_val(dictr, lst[-1])}')
