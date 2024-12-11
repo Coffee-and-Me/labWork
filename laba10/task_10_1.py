1b8388ec8257ce3c15426adfe1127395ff9650b4
Kirill_Strahov <kirich051@gmail.com>
2024-12-11 07:25:56 +0300

    upd

--- laba10/task_10_1.py
+++ laba10/task_10_1.py
@@ -16,8 +16,18 @@ def func(remv: int, file: str = "input.txt"):
     with open('output.txt', 'w') as f:
         f.write(lst)
 
-os.chdir('labWork/laba10')
-print(os.listdir(path='.'))
-print(os.path.exists('input.txt'))
+#os.chdir('labWork/laba10')
+#print(os.listdir(path='.'))
 
-func(2)
+try:
+    num = int(input('Напишите число: '))
+except:
+    print('Error')
+    exit()
+else:
+    if os.path.exists('input.txt'):
+        func(num)
+        print('Всё выполнялось корректно')
+    else:
+        print('Такого файла не существует')
+        exit()
